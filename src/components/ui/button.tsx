08ccefedb8b7836a24070ce911d719a0b83abd51
Upekka Chakma <upekkachakma@gmail.com>
2024-10-27 23:12:07 +0600

    installed appwrite for database. With sign-up and sign-in card, I can create new user and login that user. All the data are stored in the database.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -26,7 +26,7 @@ const buttonVariants = cva(
         primary: "h-10 px-4 py-2",
         sm: "h-8 rounded-md px-3",
         xs: "h-7 rounded-md px-2 text-xs",
-        lg: "h-12 rounded-md px-8 text-lg",
+        lg: "h-12 rounded-md px-8 text-base",
         icon: "h-8 w-8",
       },
     },
---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "upekka-time-loom-cookie";
