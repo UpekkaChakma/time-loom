6d3ed379c816e70eec49a5f1a788902a37f436af
Upekka Chakma <upekkachakma@gmail.com>
2024-10-27 11:23:48 +0600

    Modified UI of sign in and sign up card.

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -17,16 +17,12 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import { useLogin } from "../api/use-login";
-
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6, "At least 6 characters required.").max(256),
-});
+import { useLogin } from "@/features/auth/api/use-login";
+import { signUpSchema } from "@/features/auth/schemas";
 
 export const SignInCard = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
     defaultValues: {
       email: "",
       password: "",
@@ -35,15 +31,15 @@ export const SignInCard = () => {
 
   const { mutate } = useLogin();
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const onSubmit = (values: z.infer<typeof signUpSchema>) => {
     mutate({ json: values });
   };
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex justify-center items-center text-center px-7 pb-0">
-        <CardTitle className="text-3xl">Welcome back</CardTitle>
+        <CardTitle className="text-2xl">Login to your account</CardTitle>
       </CardHeader>
-      <CardContent className="p-7">
+      <CardContent className="p-7 pb-9">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <FormField
@@ -80,37 +76,40 @@ export const SignInCard = () => {
                 </FormItem>
               )}
             />
-            <Button disabled={false} size="lg" className="w-full">
+            <Button disabled={false} size="lg" className="w-full capitalize">
               Log in
             </Button>
           </form>
         </Form>
-        <div className="py-4 px-7">
-          <Separator />
-        </div>
-        <CardContent className="px-7 flex flex-col gap-y-4">
-          <Button
-            variant="secondary"
-            disabled={false}
-            size="lg"
-            className="w-full"
-          >
-            <FcGoogle className="mr-2 size-5" />
-            Login with Google
-          </Button>
-          <Button
-            variant="secondary"
-            disabled={false}
-            size="lg"
-            className="w-full"
-          >
-            <FaGithub className="mr-2 size-5" />
-            Login with Github
-          </Button>
-        </CardContent>
       </CardContent>
-      <CardContent>
-        <p>Don't have an account?</p>{" "}
+      <div className="px-7 relative">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white text-sm text-neutral-500 px-4">
+          or login with
+        </div>
+        <Separator />
+      </div>
+      <CardContent className="p-7 pt-9 flex flex-col gap-y-4">
+        <Button
+          variant="secondary"
+          disabled={false}
+          size="lg"
+          className="w-full font-normal"
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Sign in with Google
+        </Button>
+        <Button
+          variant="secondary"
+          disabled={false}
+          size="lg"
+          className="w-full font-normal"
+        >
+          <FaGithub className="mr-2 size-5" />
+          Sign in with Github
+        </Button>
+      </CardContent>
+      <CardContent className="flex gap-1">
+        <p>Don't have an account?</p>
         <Link href="/sign-up">
           <span className="text-blue-700"> Sign up</span>
         </Link>
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -36,13 +36,13 @@ export const SignUpCard = () => {
   });
 
   const onSubmit = (values: z.infer<typeof signUpSchema>) => {
-    console.log("");
+    console.log(values);
   };
 
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex justify-center items-center text-center px-7 pb-0 gap-3">
-        <CardTitle className="text-3xl">Sign Up</CardTitle>
+        <CardTitle className="text-2xl">Create an account</CardTitle>
         <CardDescription>
           By signing up, you agree to our{" "}
           <Link href="/privacy">
@@ -54,7 +54,7 @@ export const SignUpCard = () => {
           </Link>
         </CardDescription>
       </CardHeader>
-      <CardContent className="p-7">
+      <CardContent className="p-7 pb-9">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <FormField
@@ -109,35 +109,38 @@ export const SignUpCard = () => {
               )}
             />
             <Button disabled={false} size="lg" className="w-full">
-              Log in
+              Sign Up
             </Button>
           </form>
         </Form>
-        <div className="py-4 px-7">
-          <Separator />
-        </div>
-        <CardContent className="px-7 flex flex-col gap-y-4">
-          <Button
-            variant="secondary"
-            disabled={false}
-            size="lg"
-            className="w-full"
-          >
-            <FcGoogle className="mr-2 size-5" />
-            Login with Google
-          </Button>
-          <Button
-            variant="secondary"
-            disabled={false}
-            size="lg"
-            className="w-full"
-          >
-            <FaGithub className="mr-2 size-5" />
-            Login with Github
-          </Button>
-        </CardContent>
       </CardContent>
-      <CardContent>
+      <div className="px-7 relative">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white text-sm text-neutral-500 px-4">
+          or create an account with
+        </div>
+        <Separator />
+      </div>
+      <CardContent className="p-7 pt-9 flex flex-col gap-y-4">
+        <Button
+          variant="secondary"
+          disabled={false}
+          size="lg"
+          className="w-full font-normal"
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Sign up with Google
+        </Button>
+        <Button
+          variant="secondary"
+          disabled={false}
+          size="lg"
+          className="w-full font-normal"
+        >
+          <FaGithub className="mr-2 size-5" />
+          Sign up with Github
+        </Button>
+      </CardContent>
+      <CardContent className="flex gap-1">
         <p>Already have an account?</p>
         <Link href="/sign-in">
           <span className="text-blue-700"> Sign in</span>
